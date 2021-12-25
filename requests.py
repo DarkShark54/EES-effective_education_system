9de8163013fafb66d36aaf6028cc38757ea9d572
DarkShark54 <nikita.chertkov@yandex.ru>
2021-12-25 17:06:57 +0300

    Создан запрос и его обработка для выдачи статуса изучения темы. Также написанна обработка данных по ученикам.

--- requests.py
+++ requests.py
@@ -56,14 +56,67 @@ def get_st_marks(data_base=None, data=None):
                     FROM 
                     achievement LEFT JOIN student on student.id = achievement.id_stud
                     WHERE 
-                    achievement.id_theme = {id_theme} AND student.class = '{class_name}'""".format(id_theme=data[0],
-                                                                                                   class_name=data[1])
+                    achievement.id_theme = {id_theme} AND student.class = '{class_name}'""".format(
+                                                                                            id_theme=data[0].split(' ')[0],
+                                                                                            class_name=data[1])
     records = data_base.post_request(text_request)
     data_base.close_to_base()
+    conclusion = "  Тема: {theme}\n" \
+                 "Данная тема {status} учениками.\n" \
+                 "Рекомендованы следующие действия:\n" \
+                 "  {recommend}.\n\n" \
+                 "Ниже представлена общая успеваемость класса по данной теме:\n"
+    if not records:
+        return "Тема: {theme}\n" \
+               "Данная тема ещё не проходилась.".format(theme=data[0])
+
+    cnt = 0
+    for record in records:
+        if int(record[2]) <= 3:
+            cnt += 1
+    if cnt == 0:
+        conclusion = conclusion.format(theme=data[0], status="успешно усвоена",
+                                       recommend="Переходить к следующей теме")
+    elif cnt < len(records):
+        conclusion = conclusion.format(theme=data[0], status="частично усвоена",
+                                       recommend="Повторить ключевые моменты перед переходом к следующей теме")
+    elif cnt >= len(records):
+        conclusion = conclusion.format(theme=data[0], status="не была усвоена",
+                                       recommend="Провести дополнительное занятие по данной теме")
+    records.insert(0, conclusion)
     return records
 
 
-requests_list = {"GET": {"data_client": get_client_data, "subj_theme": get_subj_theme, "students_marks": get_st_marks},
+def get_theme_status(data_base=None, data=None):
+    data_base.connection_to_base()
+    text_request = """SELECT 
+                    achievement.id_stud, student.fio, achievement.mark
+                    FROM 
+                    achievement LEFT JOIN student on student.id = achievement.id_stud
+                    WHERE 
+                    achievement.id_theme = {id_theme} AND student.class = '{class_name}'""".format(
+                                                                                            id_theme=data[0].split(' ')[0],
+                                                                                            class_name=data[1])
+    records = data_base.post_request(text_request)
+    data_base.close_to_base()
+    conclusion = ""
+    if not records:
+        return conclusion
+
+    cnt = 0
+    for record in records:
+        if int(record[2]) <= 3:
+            cnt += 1
+    if cnt == 0:
+        conclusion = "success"
+    elif cnt < len(records):
+        conclusion = "partial success"
+    elif cnt >= len(records):
+        conclusion = "fail"
+
+    return conclusion
+
+
+requests_list = {"GET": {"data_client": get_client_data, "subj_theme": get_subj_theme,
+                         "students_marks": get_st_marks, "theme_status": get_theme_status},
                  "POST": {"authorization": client_authorization}}
-
-
--- view.py
+++ view.py
@@ -2,9 +2,5 @@ from models import data_base
 from requests import requests_list
 
 
-def index(request_type, request_name, data=0):
+def index(request_type, request_name, data=None):
     return requests_list[request_type][request_name](data_base, data)
-
-
-test = index("GET", "data_client", 1)
-print(test)
